03a82341f81c8c2d91fc2acf96884f3c2d95e4fb
chanchana <north1602@gmail.com>
2019-11-30 00:15:21 +0700

    Add login and register

---
+++ pages/user/login.vue
@@ -0,0 +1,72 @@
+<template>
+<div>
+  <div style="padding:120px 20px 20px 20px;text-align:center;">
+    <!-- <h1 class="text-center pt-4 pb-5">TAXZI</h1> -->
+    <img src="~/static/TaxziLogoGradient.png" style="height:80px">
+    <div class="d-flex justify-content-center">
+      <form class="form-horizontal" action="" method="" style="margin:120px 0px 0px 0px">
+        <!-- Username -->
+        <div class="control-group row pt-3">
+          <!-- <label class="control-label col-5" for="inputUsername">Username</label> -->
+          <input
+            class="taxzi-form input-form"
+            style="margin:auto;"
+            type="text"
+            name="inputUsername"
+            id="inputUsername"
+            placeholder="Username"
+          />
+        </div>
+        <!-- Password -->
+        <div class="control-group row pt-3">
+          <!-- <label class="control-label col-5" for="inputPassword">Password</label> -->
+          <input
+            class="taxzi-form input-form"
+            style="margin:auto;"
+            type="password"
+            name="inputPassword"
+            id="inputPassword"
+            placeholder="Password"
+          />
+        </div>
+        <div class="control-group row pt-3" style="text-align:right;">
+            <a href="register" style="color:rgb(255, 107, 48);font-weight:700;font-size:18px;padding-left:240px">Register</a>
+        </div>
+        <!-- Button -->
+        <div class="control-group row pt-5 d-flex justify-content-center">
+          <button type="submit" class="taxzi-button">Login</button>
+        </div>
+      </form>
+    </div>
+  </div>
+</div>
+</template>
+
+<style scoped>
+.bg {
+  background-color: whitesmoke;
+  padding: 0%;
+  min-height: 100%;
+  height: 100%;
+}
+.input-form {
+  height:50px;
+  width:300px;
+  padding: 20px;
+}
+.taxzi-button{
+  width               : 280px;
+  height: 56px;
+  padding             : 5px;
+  background          : rgba(255, 107, 48, 0.8);
+  border-radius       : 60px;
+  border              : 10px;
+  font-weight: 700;
+  font-size:24px;
+  color:white;
+  box-shadow: 0px 10px 30px 1px rgba(255, 107, 48, 0.5);
+  transition-duration: 0.4s;
+  cursor: pointer;
+}
+.taxzi-button:hover {background-color: rgba(255, 107, 48, 1.0);}
+</style>
---
+++ pages/user/register.vue
@@ -0,0 +1,53 @@
+<template>
+<div>
+  <div style="padding:120px 20px 20px 20px;text-align:center;">
+    <img src="~/static/TaxziLogoGradient.png" style="height:80px">
+    <div
+      style="padding:64px 38px 10px;font-size:14px;text-align:left"
+    >Please enter a valid KMUTT student or staff username (ID) and password</div>
+    <div class="d-flex justify-content-center">
+      <form class="form-horizontal" action method>
+        <!-- Username -->
+        <div class="control-group row pt-3">
+          <!-- <label class="control-label col-5" for="inputUsername">Username</label> -->
+          <input
+            class="taxzi-form"
+            type="text"
+            name="inputUsername"
+            id="inputUsername"
+            placeholder="Username"
+          />
+        </div>
+        <!-- Password -->
+        <div class="control-group row pt-3">
+          <!-- <label class="control-label col-5" for="inputPassword">Password</label> -->
+          <input
+            class="taxzi-form"
+            type="password"
+            name="inputPassword"
+            id="inputPassword"
+            placeholder="Password"
+          />
+        </div>
+      </form>
+    </div>
+    <button
+      class="taxzi-button"
+      style="margin:24px 0px 10px"
+    >Next</button>
+    <div style="padding:18px 38px 24px;font-size:14px;text-align:left">
+      If you already have Taxzi account, you can
+      <a href="login" style="color:rgb(255, 107, 48); font-weight:700;">Login</a>
+    </div>
+  </div>
+</div>
+</template>
+
+<style scoped>
+.bg {
+  background-color: whitesmoke;
+  padding: 0%;
+  min-height: 100%;
+  height: 100%;
+}
+</style>
